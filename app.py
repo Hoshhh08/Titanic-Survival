41731e6747d396f4a877ebe4888e97a4db5f5f16
Hoshang Sheth <168591647+Hoshhh08@users.noreply.github.com>
2025-02-04 16:37:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle as pkl
+from sklearn.preprocessing import StandardScaler
+
+
+# Load trained model file:
+
+with open('D:/AssignmentDatasets/Logistic Regression/trained_logi_model.pkl','rb') as model_file:
+    model = pkl.load(model_file)
+
+with open('D:/AssignmentDatasets/Logistic Regression/scaler.pkl','rb') as scaler_file:
+    scaler = pkl.load(scaler_file)
+
+
+# Streamlit UI Setup:
+st.set_page_config(page_title='Survival Quest: Titanic Edition',layout='centered')
+
+st.title('Titanic Survival Predictor')
+st.markdown('### Choose the features from the pop-up window to predict survival.')
+
+
+# Initialize session state for feature values:
+if 'features' not in st.session_state:
+    st.session_state.features = {
+        'Age': 20,
+        'Sibsp':0,
+        'Parch':0,
+        'Sex_female':0,
+        'Embarked_C':0,
+        'WealthScore':75, # Default Value of Pclass*fare
+    }
+
+
+# Pop-up for Feature Selection:
+with st.popover('Select Features'):
+    age = st.slider('Age',1,100,st.session_state.features['Age'])
+    sibsp = st.number_input('Siblings/Spouses Aboard',0,10,st.session_state.features['Sibsp'])
+    parch = st.number_input('Parents/Children Aboard',0,10,st.session_state.features['Parch'])
+    gender = st.radio('Gender',['Male','Female'])
+    embarked = st.radio('Embarked at Cherbourg?',['No','Yes'])
+    wealthscore = st.number_input('WealthScore[Pclass*Fare]',0,600,st.session_state.features['WealthScore'])
+
+    if st.button('Apply Features'):
+        st.session_state.features = {
+            "Age": age,
+            "SibSp": sibsp,
+            "Parch": parch,
+            "Sex_female": 1 if gender == "Female" else 0,
+            "Embarked_C": 1 if embarked == "Yes" else 0,
+            "WealthScore": wealthscore,
+        }
+        st.rerun()  # Refresh UI to apply changes
+
+
+# Display Selected Features
+st.write('### Selected Features:')
+for key, value in st.session_state.features.items():
+    st.write(f'- **{key}:**,{value}')
+
+
+# Prediction Button
+if st.button('Predict Surival'):
+    # convert features into DataFrame
+    input_data = pd.DataFrame([st.session_state.features])
+
+    #Standardize features
+    input_scaled = scaler.transform(input_data)
+
+    #Predict Survival
+    prediction = model.predict(input_scaled)[0]
+
+    #Display unit
+    result = "Survived 🎉" if prediction == 1 else "Did Not Survive 😔"
+    st.subheader(f"Prediction: {result}")
